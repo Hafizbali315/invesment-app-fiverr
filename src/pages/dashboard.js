1982c69b5df9b5ed4144b7e06d7f0fead2a7c398
hafizbali315 <hafizbali315@gmail.com>
2021-12-01 15:11:11 +0500

    Dashboard Page

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,5 +1,31 @@
+import { NewsFeed, TaskListCard } from '../components/HomePage'
+import VentureSummeryCard from './../components/VentureSummeryCard/index'
+
 const DashboardPage = () => {
-	return <div>DashboardPage</div>
+	return (
+		<div className="dashboard_page container-fluid">
+			<div className=" row">
+				<div className="col-lg-4">
+					<div className="buttons_row">
+						<button>Retail</button>
+						<button>Transport</button>
+						<button>Agriculture</button>
+					</div>
+				</div>
+			</div>
+
+			<VentureSummeryCard />
+
+			<div className="row">
+				<div className="col-lg-7">
+					<NewsFeed />
+				</div>
+				<div className="col-lg-5">
+					<TaskListCard />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default DashboardPage
